b9d93d543c1591b09f608e002bda1da2360fb871
Nur <nur@nurdreyer.com>
2023-12-11 12:15:13 -0800

    Add login form fields and styling

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
 
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup
+
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl(''),
+    });
+  }
 }
