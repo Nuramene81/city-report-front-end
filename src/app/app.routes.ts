4c08164048675e5948d94f644989ea6874410294
Nuramene81 <116958206+Nuramene81@users.noreply.github.com>
2023-12-15 12:55:38 -0800

    Add sign up page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 export const routes: Routes = [
     {
@@ -11,5 +12,10 @@ export const routes: Routes = [
     {
         path: 'login',
         component: LoginComponent
+    },
+    {
+        path: 'sign-up',
+        component: SignUpComponent
     }
+
 ];
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -5,25 +5,29 @@ import { LogoComponent } from '../logo/logo.component';
 import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-sign-up',
   standalone: true,
   imports: [
     RouterModule,
     ReactiveFormsModule,
     LogoComponent
   ],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  templateUrl: './sign-up.component.html',
+  styleUrl: './sign-up.component.scss'
 })
-
-export class LoginComponent implements OnInit {
+export class SignUpComponent implements OnInit {
   loginForm!: FormGroup
   showPassword = false;
+  showConfirmPassword = false;
 
   ngOnInit() {
     this.loginForm = new FormGroup({
       email: new FormControl(''),
-      password: new FormControl('')
+      fullName: new FormControl(''),
+      username: new FormControl(''),
+      password: new FormControl(''),
+      confirmPassword: new FormControl('')
     });
   }
+
 }
