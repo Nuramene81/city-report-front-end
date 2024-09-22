eec7fcadac91bf7e0f24d03e6e532b46c0a62144
Nuramene81 <nur@nurdreyer.com>
2024-09-22 22:47:52 +0200

    new URL

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const GOOGLE_MAPS_API_KEY = 'AIzaSyBgZdhpkrKU58GzW4CTSHdgGsXd34E1tY0';
 export const urls = {
   devBEAppURL: 'http://localhost:3000',
-  prodBEAppURL: 'https://city-report-backend-v2.vercel.app'
+  prodBEAppURL: 'https://city-report-backend-v3.onrender.com'
 };
 export const useURL = urls.prodBEAppURL;
