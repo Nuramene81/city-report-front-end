8a108ac3b1d14995462efeed4ddf0aea25cce61b
NurDreyer <nur.dreyer@sebrio.com>
2024-09-22 12:18:09 +0200

    switch to prod

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const GOOGLE_MAPS_API_KEY = 'AIzaSyBgZdhpkrKU58GzW4CTSHdgGsXd34E1tY0';
 export const urls = {
   devBEAppURL: 'http://localhost:3000',
-  prodBEAppURL: 'https://city-report-be.onrender.com'
+  prodBEAppURL: 'https://city-report-backend-v2.onrender.com'
 };
-export const useURL = urls.devBEAppURL;
+export const useURL = urls.prodBEAppURL;
